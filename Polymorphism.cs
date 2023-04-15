b0d88cd12b1230d5ac61494392059ad9dbd21d8c
sonali4596 <sonalianmol4596@gmail.com>
2023-04-15 11:58:03 +0530

    Update Polymorphism.cs

--- Polymorphism.cs
+++ Polymorphism.cs
@@ -1,1 +1,9 @@
+/**
+Polymorphism enables us to invoke derive class methods using base class reference variable at runtime
+Polymorphism allows us to invoke a derived class method through a base class reference during runtime.
+The virtual keyword declares that the method can be overridden in any base class
+there Are two types of polymorphism :
+1. Runtime time Polymorphism : method overriding  also known as dynamic binding or late binding
+2. compile Time polymorphism: method  and operator overloading  also known as static binding or early binding
 
+**/
