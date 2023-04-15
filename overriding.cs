a426783434a1976f51e8d94e8865941519e637aa
sonali4596 <sonalianmol4596@gmail.com>
2023-04-15 09:58:39 +0530

    Create overriding.cs

---
+++ overriding.cs
@@ -0,0 +1,5 @@
+/**
+the virtual keyword in base class gives the child class option that if they want they can override the method and if they don't want
+they can choose not to implement They Don't force child class to override.
+
+**/
